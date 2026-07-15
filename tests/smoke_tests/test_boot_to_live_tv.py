763b4c5493e4d855466c654d5b06e914b11aac05
Sathish S <sathish.s@ovtec.in>
2026-07-15 12:51:38 +0530

    Updated the test case - test_boot_to_live_tv.py

--- tests/smoke_tests/test_boot_to_live_tv.py
+++ tests/smoke_tests/test_boot_to_live_tv.py
@@ -1,23 +1,24 @@
+import time
 import pytest
 
 from core.logger import logging
 
 logger = logging.getLogger(__name__)
 
-
 class Test_Boot_to_Live_TV:
-    
-    def test_boot_to_live(self, boot):
-        status, tl_boot_time, results = boot.check_boot_sequence()
+
+    def test_bootup(self, stb):
+        status, tl_boot_time, results = stb.boot.check_boot_sequence()
         logger.info("Total boot time: %d sec", tl_boot_time)
         logger.info("Results: %s", results)
         assert status
 
-    """
-    def test_af_boot_is_live_tv_playing(self):
-        pass
+    def test_channel_banner_present(self, stb):
+        status, lcn, sname = stb.channel_banner.is_channel_banner_present()
+        if status:
+            logger.info("LCN: %d", lcn)
+            logger.info("SERVICE NAME: %s", sname)
+        assert status
 
-    def test_total_boot_time(self):
-        pass
-        
-    """
+    def test_live_tv(self, stb):
+        assert stb.liveTV.is_video_alive(duration=15)
